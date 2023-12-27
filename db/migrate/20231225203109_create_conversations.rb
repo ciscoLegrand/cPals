b81ce7c3decea9ec59f641e8f10c64f03ab6805b
cisco gonzalez <cisco.gonzalez@blipol.es>
2023-12-27 19:33:20 +0100

    Add interactions association and scopes to Conversation model, and modify conversations table structure

--- db/migrate/20231225203109_create_conversations.rb
+++ db/migrate/20231225203109_create_conversations.rb
@@ -1,9 +1,9 @@
 class CreateConversations < ActiveRecord::Migration[7.1]
   def change
-    create_table :conversations do |t|
+    create_table :conversations, id: :uuid do |t|
       t.integer :user_id
+      t.string :ip_address
       t.string :title
-      t.text :description
 
       t.timestamps
     end
