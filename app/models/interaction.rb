b1ce78c0b781da82c09ace80dcb264326d5b2b29
cisco gonzalez <cisco.gonzalez@blipol.es>
2023-12-27 19:34:53 +0100

    Add CustomRender class and answer_text_to_html method to Interaction model Add additional fields for API information in interactions table Create new schema.rb file

--- app/models/interaction.rb
+++ app/models/interaction.rb
@@ -1,3 +1,25 @@
 class Interaction < ApplicationRecord
   belongs_to :conversation
+
+  # Define la clase de renderizado personalizado dentro de Interaction
+  class CustomRender < Redcarpet::Render::HTML
+    def initialize(view_context, options = {})
+      super(options)
+      @view_context = view_context
+    end
+
+    def block_code(code, language)
+      UI::Terminal.new(code: CGI.escapeHTML(code), language: language).render_in(@view_context)
+    end
+
+    def codespan(code)
+      "<code class='px-4 py-1 bg-black text-white rounded-lg'>#{CGI.escapeHTML(code)}</code>"
+    end
+  end
+
+  def answer_text_to_html(view_context)
+    renderer = CustomRender.new(view_context, hard_wrap: true, filter_html: true)
+    markdown = Redcarpet::Markdown.new(renderer, fenced_code_blocks: true, no_intra_emphasis: true, autolink: true, tables: true, lax_spacing: true)
+    markdown.render(answer_text).html_safe
+  end
 end
