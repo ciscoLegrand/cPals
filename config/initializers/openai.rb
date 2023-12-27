257eb52bb42a174d3c0733a4fdd814abe1d7d860
cisco gonzalez <cisco.gonzalez@blipol.es>
2023-12-27 19:32:02 +0100

    Add gems: nokogiri, httparty, ruby-openai, view_component, inline_svg, redcarpet

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,4 +1,4 @@
 OpenAI.configure do |config|
-  config.access_token = Rails.application.credentials.dig(:openai_access_token)
+  config.access_token = Rails.application.credentials.openai_acces_token
   # config.organization_id = ENV.fetch("OPENAI_ORGANIZATION_ID") # Optional.
 end
