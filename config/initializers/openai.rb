fa712600ab41c455c207b04b20f7bbc98f7a097d
cisco gonzalez <cisco.gonzalez@blipol.es>
2024-02-28 22:04:57 +0100

    Update credentials

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 OpenAI.configure do |config|
-  config.access_token = ENV.fetch("OPENAI_ACCESS_KEY", Rails.application.credentials.OPENAI_ACCESS_KEY)
+  config.access_token = Rails.application.credentials.dig(:openai, :access_token)
   # config.organization_id = ENV.fetch("OPENAI_ORGANIZATION_ID") # Optional.
 end
